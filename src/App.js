a81339750dffa996b71e8c561760a711a9e0228b
worldsong <2060884089@qq.com>
2018-04-13 20:28:36 +0800

    引入Header组件和布局样式。

--- src/App.js
+++ src/App.js
@@ -1,19 +1,17 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { Layout } from 'antd';
+import './style/index.less';
+
+import HeaderCustom from './components/HeaderCustom';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
+        <Layout className="ant-layout-has-sider">
+            <Layout>
+                <HeaderCustom />
+            </Layout>
+        </Layout>
     );
   }
 }
