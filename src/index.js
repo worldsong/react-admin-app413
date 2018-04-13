8e1314e581810fa25e2c06441c2fc099d31687a3
worldsong <2060884089@qq.com>
2018-04-13 22:29:00 +0800

    引入表格和面包屑组件。

--- src/index.js
+++ src/index.js
@@ -6,6 +6,8 @@ import { Router, Route, hashHistory, IndexRedirect } from 'react-router';
 import BasicForm from './components/forms/BasicForm';
 import Page from './components/Page'
 import WrappedForm from './components/forms/WrappedForm'
+import BasicTable from './components/tables/BasicTables'
+import AdvancedTable from './components/tables/AdvancedTables'
 
 ReactDOM.render(
   <Router history={hashHistory}>
@@ -15,6 +17,10 @@ ReactDOM.render(
               <Route path={'form'}>
                   <Route path={'basicform'} components={BasicForm}></Route>
                   <Route path={'wrappedform'} components={WrappedForm}></Route>
+              </Route>
+              <Route path={'table'}>
+                  <Route path={'basicTable'} component={BasicTable} />
+                  <Route path={'advancedTable'} components={AdvancedTable} />
               </Route>
           </Route>
       </Route>
