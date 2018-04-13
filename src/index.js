30f9f4c4663804f5c2986194283b3d342189027e
worldsong <2060884089@qq.com>
2018-04-13 20:47:01 +0800

    引入路由。

--- src/index.js
+++ src/index.js
@@ -2,8 +2,12 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 import './index.css';
+import { Router, Route, hashHistory, IndexRedirect } from 'react-router';
 
 ReactDOM.render(
-  <App />,
+  <Router history={hashHistory}>
+      <Route path={'app'} component={App}>
+      </Route>
+  </Router>,
   document.getElementById('root')
 );
