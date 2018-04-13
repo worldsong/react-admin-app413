d1169c6fd564b9cb154d0949b1a0e4d8a2495f67
worldsong <2060884089@qq.com>
2018-04-13 20:38:49 +0800

    引入侧边导航，后台管理系统页面布局。

--- src/App.js
+++ src/App.js
@@ -1,15 +1,25 @@
 import React, { Component } from 'react';
 import { Layout } from 'antd';
+const { Content, Footer } = Layout;
+
 import './style/index.less';
 
 import HeaderCustom from './components/HeaderCustom';
+import SiderCustom from './components/SiderCustom';
 
 class App extends Component {
   render() {
     return (
         <Layout className="ant-layout-has-sider">
+            <SiderCustom />
             <Layout>
                 <HeaderCustom />
+                <Content style={{ margin: '0 16px', overflow: 'initial' }}>
+                    Hello
+                </Content>
+                <Footer style={{ textAlign: 'center' }}>
+                    Ant Design ©2016 Created by Ant UED
+                </Footer>
             </Layout>
         </Layout>
     );
---
+++ src/components/SiderCustom.jsx
@@ -0,0 +1,87 @@
+import React, { Component } from 'react';
+import { Layout, Menu, Icon } from 'antd';
+const { Sider } = Layout;
+const SubMenu = Menu.SubMenu;
+import { Link } from 'react-router';
+
+class SiderCustom extends Component {
+    state = {
+        collapsed: false,
+        mode: 'inline',
+        openKey: '',
+        selectedKey: ''
+    };
+    componentDidMount() {
+        const _path = this.props.path;
+        this.setState({
+            openKey: _path.substr(0, _path.lastIndexOf('/')),
+            selectedKey: _path
+        });
+    }
+    onCollapse = (collapsed) => {
+        console.log(collapsed);
+        this.setState({
+            collapsed,
+            mode: collapsed ? 'vertical' : 'inline',
+        });
+    };
+    menuClick = e => {
+        this.setState({
+            selectedKey: e.key
+        });
+        console.log(this.state);
+
+    };
+    openMenu = v => {
+        console.log(v);
+        this.setState({
+            openKey: v[v.length - 1]
+        })
+    };
+    render() {
+        return (
+            <Sider
+                breakpoint="lg"
+                collapsible
+                collapsed={this.state.collapsed}
+                onCollapse={this.onCollapse}
+            >
+                <div className="logo" />
+                <Menu
+                    onClick={this.menuClick}
+                    theme="dark"
+                    mode={this.state.mode}
+                    selectedKeys={[this.state.selectedKey]}
+                    openKeys={[this.state.openKey]}
+                    onOpenChange={this.openMenu}
+                >
+                    <SubMenu
+                        key="/app/table"
+                        title={<span><Icon type="copy" /><span className="nav-text">表格</span></span>}
+                    >
+
+                        <Menu.Item key="/app/table/basicTable"><Link to={'/app/table/basicTable'}>基础表格</Link></Menu.Item>
+                        <Menu.Item key="/app/table/advancedTable"><Link to={'/app/table/advancedTable'}>高级表格</Link></Menu.Item>
+                    </SubMenu>
+                    <SubMenu
+                        key="/app/form"
+                        title={<span><Icon type="edit" /><span className="nav-text">表单</span></span>}
+                    >
+
+                        <Menu.Item key="/app/basicForm"><Link to={'/app/form/basicForm'}>基础表单</Link></Menu.Item>
+                    </SubMenu>
+                </Menu>
+                <style>
+                    {`
+                    #nprogress .spinner{
+                        left: ${this.state.collapsed ? '70px' : '206px'};
+                        right: 0 !important;
+                    }
+                    `}
+                </style>
+            </Sider>
+        )
+    }
+}
+
+export default SiderCustom;
