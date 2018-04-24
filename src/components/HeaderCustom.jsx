8cda28fa7e2686b2a635f0aaabe27ce6ef94b666
worldsong <2060884089@qq.com>
2018-04-24 21:34:03 +0800

    全屏功能实现screenfull，用户登录状态部分实现，后期使用redux。

--- src/components/HeaderCustom.jsx
+++ src/components/HeaderCustom.jsx
@@ -1,10 +1,43 @@
 import React, { Component } from 'react';
 import { Menu, Icon, Layout, Badge } from 'antd';
+import screenfull from 'screenfull';
+import { gitOauthToken, gitOauthInfo } from '../axios';
+import { queryString } from '../utils/index';
+import avater from '../style/imgs/b1.jpg';
+
 const { Header } = Layout;
 const SubMenu = Menu.SubMenu;
 const MenuItemGroup = Menu.ItemGroup;
 
 class HeaderCustom extends Component {
+    state = {
+        user: ''
+    };
+    componentDidMount() {
+        const QueryString = queryString();
+        const _user = JSON.parse(localStorage.getItem('user')) || '测试';
+        if (!_user && QueryString.hasOwnProperty('code')) {
+            gitOauthToken(QueryString.code).then(res => {
+                gitOauthInfo(res.access_token).then(info => {
+                    console.log(info);
+                    this.setState({
+                        user: info
+                    });
+                    localStorage.setItem('user', JSON.stringify(info));
+                });
+            });
+        } else {
+            this.setState({
+                user: _user
+            });
+        }
+    };
+    screenFull = () => {
+        if (screenfull.enabled) {
+            screenfull.request();
+        }
+    };
+
     render() {
         return (
             <Header style={{ background: '#fff', padding: 0, height: 65 }} >
@@ -12,14 +45,18 @@ class HeaderCustom extends Component {
                     mode="horizontal"
                     style={{ lineHeight: '64px', float: 'right' }}
                 >
+                    <Menu.Item key="full" onClick={this.screenFull} >
+                        <Icon type="arrows-alt" onClick={this.screenFull} />
+                    </Menu.Item>
                     <Menu.Item key="1">
                         <Badge count={25} overflowCount={10} style={{marginLeft: 10}}>
                             <Icon type="notification" />
                         </Badge>
                     </Menu.Item>
-                    <SubMenu title={<span><Icon type="user" />你好 - WorldSong</span>}>
+                    <SubMenu title={<span className="avatar"><img src={avater} alt="头像" /><i className="on bottom b-white" /></span>}>
                         <MenuItemGroup title="用户中心">
-                            <Menu.Item key="setting:1">个人信息</Menu.Item>
+                            <Menu.Item key="setting:1">你好 - {this.state.user.login}</Menu.Item>
+                            <Menu.Item key="setting:2">个人信息</Menu.Item>
                         </MenuItemGroup>
                         <MenuItemGroup title="设置中心">
                             <Menu.Item key="setting:3">个人设置</Menu.Item>
@@ -27,6 +64,12 @@ class HeaderCustom extends Component {
                         </MenuItemGroup>
                     </SubMenu>
                 </Menu>
+                <style>{`
+                    .ant-menu-submenu-horizontal > .ant-menu {
+                        width: 120px;
+                        left: -40px;
+                    }
+                `}</style>
             </Header>
         )
     }
